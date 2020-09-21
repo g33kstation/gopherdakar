7767f98d918e22e5636d9c7618df565227c60a05
Pathe SENE <psene@boaholding.com>
2020-09-21 21:24:13 +0000

    declaration de la structure

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ var age int = 23           // 0
 var budget float64         // 0.0 (zeroed value)
 var bogoss bool            // false (zeroed value)
 
+//déclaration d'une structure
+type person struct {
+	nom, prenom string
+	age         int
+}
+
 func main() {
+
+	Omar := person{"FAll", "Omar", 42}
 	// Go est compatible utf-8 par default (caratère arabe, chinois, emojis)
 	fmt.Println("Hello world 😋")
 
@@ -18,4 +26,5 @@ func main() {
 	fmt.Printf("%#V\n", age)
 
 	fmt.Printf("%#V\n", bogoss)
+	fmt.Printf("%#V\n", Omar)
 }
