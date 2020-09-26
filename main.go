d0ad76e07ef1014c0cc9c56b34203e94fecce9d7
Oumar FALL <oumarfall@hotmail.fr>
2020-09-26 16:17:07 +0100

    play with the Stringer interface
    https://golang.org/pkg/fmt/#Stringer

--- main.go
+++ main.go
@@ -3,28 +3,19 @@ package main
 
 import "fmt"
 
-// declaration de variables globales
-// suivi
-var prenom string = "Issa" // ""
-var age int = 23           // 0
-var budget float64         // 0.0 (zeroed value)
-var bogoss bool            // false (zeroed value)
-
-//déclaration d'une structure
 type person struct {
 	nom, prenom string
 	age         int
 }
 
-func main() {
+func (p person) String() string {
+	return fmt.Sprintf("L'utilisateur est %s %s, et il a %d ans!\n", p.prenom, p.nom, p.age)
+}
 
+func main() {
 	Omar := person{"FAll", "Omar", 42}
 	// Go est compatible utf-8 par default (caratère arabe, chinois, emojis)
 	fmt.Println("Hello world 😋")
 
-	fmt.Printf("%#V\n", prenom)
-	fmt.Printf("%#V\n", age)
-
-	fmt.Printf("%#V\n", bogoss)
-	fmt.Printf("%#V\n", Omar)
+	fmt.Println(Omar)
 }
