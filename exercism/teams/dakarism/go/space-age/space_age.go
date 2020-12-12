a8ea2b9ad6d5263cc7746e3e0947244e2f125dff
Oumar FALL <oumarfall@hotmail.fr>
2020-12-12 14:46:34 +0100

    added the bare minimum to start the problem resolution

--- exercism/teams/dakarism/go/space-age/space_age.go
+++ exercism/teams/dakarism/go/space-age/space_age.go
@@ -1,3 +1,10 @@
+// Package space should calculate one's age on a given planet
 package space
 
+// Planet define a solar system planet
 type Planet string
+
+// Age should calculate how old someone would be on a given planet
+func Age(seconds float64, planet Planet) float64 {
+	return 0.0
+}
