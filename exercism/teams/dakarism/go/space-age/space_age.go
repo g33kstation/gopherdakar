026353d5f2eed6d4ddb13155efb70744590b840b
Oumar FALL <oumarfall@hotmail.fr>
2020-12-14 11:50:47 +0100

    finalize the age calculation formula

--- exercism/teams/dakarism/go/space-age/space_age.go
+++ exercism/teams/dakarism/go/space-age/space_age.go
@@ -16,7 +16,8 @@ var orbitalPeriod = map[Planet]float64{
 	"Uranus":  84.016846,
 	"Neptune": 164.79132,
 }
+
 // Age should calculate how old someone would be on a given planet
 func Age(seconds float64, planet Planet) float64 {
-	return 0.0
+	return seconds / earthYear / orbitalPeriod[planet]
 }
