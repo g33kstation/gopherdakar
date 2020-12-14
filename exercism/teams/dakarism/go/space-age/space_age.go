0ef57eeb481a4e29816cd0af76a60d2d1407b3af
Oumar FALL <oumarfall@hotmail.fr>
2020-12-14 11:47:27 +0100

    add map of Orbital period of each planet in Earth Year

--- exercism/teams/dakarism/go/space-age/space_age.go
+++ exercism/teams/dakarism/go/space-age/space_age.go
@@ -4,6 +4,17 @@ package space
 // Planet define a solar system planet
 type Planet string
 
+
+var orbitalPeriod = map[Planet]float64{
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1.0,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
 // Age should calculate how old someone would be on a given planet
 func Age(seconds float64, planet Planet) float64 {
 	return 0.0
