0c13652b8fbb02da771937d1be85263b043f6be9
Oumar FALL <oumarfall@hotmail.fr>
2020-12-14 12:47:04 +0100

    Ready to start the exercise

--- exercism/teams/dakarism/go/accumulate/accumulate.go
+++ exercism/teams/dakarism/go/accumulate/accumulate.go
@@ -1,1 +1,9 @@
 package accumulate
+
+// Converter is a function that do things
+type Converter func(string) string
+
+// Accumulate perform an operation on each element a collection
+func Accumulate(given []string, converter Converter) []string {
+	return []string{}
+}
